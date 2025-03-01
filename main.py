4f7967f5c4c85c23382cee4ff6e1b28735014938
y-syuger <9201abcd@gmail.com>
2025-03-02 05:01:58 +0900

    いろいろやった

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-import matplotlib.pyplot as plt
+from flask import Flask, render_template, request, jsonify
+import requests
+import json
 
-print("Hello,Python")
+app = Flask(__name__)
+
+# Jsonから問題を取得
+def load_questions():
+    with open('questions/questions.json', 'r', encoding='utf-8') as f:
+        questions = json.load(f)
+        return questions
+
+@app.route('/')
+def index():
+    questions_data = load_questions()
+    return render_template('index.html', questions=questions_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
